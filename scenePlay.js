e177b9250016594645ecd5f862152f58d90fe795
fortin0o <donaldterrifortino@gmail.com>
2026-05-22 19:26:31 +0700

    refactor: implement flappy-style physics and update scene mechanics in scenePlay

--- scenePlay.js
+++ scenePlay.js
@@ -1,10 +1,11 @@
 var scenePlay = new Phaser.Class({
     Extends: Phaser.Scene,
-    initialize: function () {
+
+    initialize: function ScenePlay() {
         Phaser.Scene.call(this, { key: 'scenePlay' });
     },
 
-    init: function () {},
+    init: function () { },
 
     preload: function () {
         this.load.image('chara', 'assets/images/chara.png');
@@ -12,116 +13,52 @@ var scenePlay = new Phaser.Class({
         this.load.image('fg_loop', 'assets/images/fg_loop.png');
         this.load.image('obstc', 'assets/images/obstc.png');
         this.load.image('panel_skor', 'assets/images/panel_skor.png');
+
         this.load.audio('snd_dead', 'assets/audio/dead.mp3');
-        this.load.audio('snd_klik1', 'assets/audio/klik_1.mp3');
-        this.load.audio('snd_klik2', 'assets/audio/klik_2.mp3');
-        this.load.audio('snd_klik3', 'assets/audio/klik_3.mp3');
+        this.load.audio('snd_klik_1', 'assets/audio/klik_1.mp3');
+        this.load.audio('snd_klik_2', 'assets/audio/klik_2.mp3');
+        this.load.audio('snd_klik_3', 'assets/audio/klik_3.mp3');
     },
 
     create: function () {
-        this.snd_dead = this.sound.add('snd_dead');
+        var myScene = this;
 
-        this.snd_click = [];
-        this.snd_click.push(this.sound.add('snd_klik1'));
-        this.snd_click.push(this.sound.add('snd_klik2'));
-        this.snd_click.push(this.sound.add('snd_klik3'));
+        this.gameWidth = 1024;
+        this.gameHeight = 768;
 
-        for (let i = 0; i < this.snd_click.length; i++) {
-            this.snd_click[i].setVolume(0.5);
-        }
-
-        this.timerHalangan = 0;
+        this.timerHalangan = 80;
         this.halangan = [];
         this.background = [];
 
         this.isGameRunning = false;
-
-        // --- Physics variables ---
-        this.GROUND_Y = 690;
-        this.GRAVITY = 0.8;       // pixels per frame² (tunable)
-        this.JUMP_FORCE = -16;    // negative = upward (tunable)
-        this.velocityY = 0;
-        this.isOnGround = false;
-
-        this.chara = this.add.image(130, this.GROUND_Y, 'chara');
-        this.chara.setDepth(3);
-        this.chara.setScale(0);
-
-        var myScene = this;
-
-        this.tweens.add({
-            delay: 250,
-            targets: this.chara,
-            ease: 'Back.Out',
-            duration: 500,
-            scaleX: 1,
-            scaleY: 1,
-            onComplete: function () {
-                myScene.isGameRunning = true;
-            }
-        });
+        this.isGameOver = false;
 
         this.score = 0;
 
-        this.panel_score = this.add.image(1024 / 2, 60, 'panel_skor');
-        this.panel_score.setOrigin(0.5);
-        this.panel_score.setDepth(10);
-        this.panel_score.setAlpha(0.8);
+        // Physics versi terbang / flappy
+        this.gravity = 0.38;
+        this.jumpPower = -8.2;
+        this.charaVelocity = 0;
+        this.maxFallSpeed = 8.5;
+        this.maxUpSpeed = -9;
 
-        this.label_score = this.add.text(this.panel_score.x + 25, this.panel_score.y, this.score, {
-            fontSize: '30px',
-            color: '#ff732e',
-            fontFamily: 'Arial'
-        });
-        this.label_score.setOrigin(0.5);
-        this.label_score.setDepth(10);
-        this.label_score.setFontSize(30);
-        this.label_score.setTint(0xff732e);
+        this.minSpawnDelay = 85;
+        this.maxSpawnDelay = 135;
+        this.baseObstacleSpeed = 5.2;
 
-        // --- Add control hint text ---
-        this.hintText = this.add.text(1024 / 2, 768 - 30, 'TAP / CLICK / SPACE / ↑ to Jump', {
-            fontSize: '16px',
-            color: '#ffffff',
-            fontFamily: 'Arial',
-            alpha: 0.6
-        });
-        this.hintText.setOrigin(0.5);
-        this.hintText.setDepth(10);
-        this.hintText.setAlpha(0.6);
+        this.topLimit = 35;
+        this.bottomLimit = 710;
 
-        this.gameover = function () {
-            let highScore = 0;
-            try {
-                highScore = localStorage.getItem('highscore') || 0;
-            } catch (e) {
-                highScore = 0;
-            }
+        this.snd_dead = this.sound.add('snd_dead');
 
-            if (myScene.score > highScore) {
-                try {
-                    localStorage.setItem('highscore', myScene.score);
-                } catch (e) {}
-            }
-            myScene.scene.start('sceneMenu');
-        };
+        this.snd_click = [];
+        this.snd_click.push(this.sound.add('snd_klik_1'));
+        this.snd_click.push(this.sound.add('snd_klik_2'));
+        this.snd_click.push(this.sound.add('snd_klik_3'));
 
-        // --- Jump function (called from any input) ---
-        this.doJump = function () {
-            if (!myScene.isGameRunning) return;
-            if (!myScene.isOnGround) return;    // only jump when grounded
-            myScene.velocityY = myScene.JUMP_FORCE;
-            myScene.isOnGround = false;
-            myScene.snd_click[Math.floor(Math.random() * 3)].play();
-        };
-
-        // Pointer / touch input
-        this.input.on('pointerdown', function () {
-            myScene.doJump();
-        });
-
-        // Keyboard input: Spacebar or Arrow Up
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        for (let i = 0; i < this.snd_click.length; i++) {
+            this.snd_click[i].setVolume(0.45);
+        }
 
         var bg_x = 1366 / 2;
 
@@ -133,6 +70,8 @@ var scenePlay = new Phaser.Class({
 
             BG.setData('kecepatan', 2);
             FG.setData('kecepatan', 4);
+
+            BG.setDepth(0);
             FG.setDepth(2);
 
             bg_awal.push(BG);
@@ -142,116 +81,249 @@ var scenePlay = new Phaser.Class({
 
             bg_x += 1366;
         }
+
+        this.chara = this.add.image(130, 768 / 2, 'chara');
+        this.chara.setDepth(3);
+        this.chara.setScale(0);
+        this.chara.setAlpha(1);
+
+        this.charaTweens = null;
+
+        this.tweens.add({
+            delay: 250,
+            targets: this.chara,
+            ease: 'Back.Out',
+            duration: 500,
+            scaleX: 1,
+            scaleY: 1,
+            onComplete: function () {
+                myScene.isGameRunning = true;
+                myScene.charaVelocity = 0;
+            }
+        });
+
+        this.panel_score = this.add.image(1024 / 2, 60, 'panel_skor');
+        this.panel_score.setOrigin(0.5);
+        this.panel_score.setDepth(10);
+        this.panel_score.setAlpha(0.85);
+
+        this.label_score = this.add.text(this.panel_score.x + 25, this.panel_score.y, this.score.toString(), {
+            fontFamily: 'Arial',
+            fontSize: '30px',
+            fontStyle: 'bold',
+            color: '#ff732e'
+        });
+
+        this.label_score.setOrigin(0.5);
+        this.label_score.setDepth(10);
+
+        this.guideText = this.add.text(1024 / 2, 720, 'Klik / tekan SPACE untuk naik', {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            fontStyle: 'bold',
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 5
+        });
+
+        this.guideText.setOrigin(0.5);
+        this.guideText.setDepth(20);
+
+        this.tweens.add({
+            targets: this.guideText,
+            alpha: 0.3,
+            duration: 700,
+            yoyo: true,
+            repeat: -1
+        });
+
+        this.input.on('pointerdown', function () {
+            myScene.flap();
+        });
+
+        this.input.keyboard.on('keydown-SPACE', function () {
+            myScene.flap();
+        });
+
+        this.input.keyboard.on('keydown-UP', function () {
+            myScene.flap();
+        });
+
+        this.flap = function () {
+            if (!this.isGameRunning || this.isGameOver) return;
+
+            if (this.charaTweens != null) {
+                this.charaTweens.stop();
+            }
+
+            this.charaVelocity = this.jumpPower;
+
+            let randomClick = Math.floor(Math.random() * this.snd_click.length);
+            this.snd_click[randomClick].play();
+
+            this.tweens.add({
+                targets: this.chara,
+                angle: -18,
+                duration: 120,
+                ease: 'Power1'
+            });
+        };
+
+        this.spawnObstacle = function () {
+            if (!this.isGameRunning || this.isGameOver) return;
+
+            let speedBonus = Math.min(this.score * 0.08, 3.2);
+            let obstacleSpeed = this.baseObstacleSpeed + speedBonus;
+
+            let acak_y = Phaser.Math.Between(120, 630);
+
+            let halanganBaru = this.add.image(1120, acak_y, 'obstc');
+            halanganBaru.setOrigin(0.5);
+            halanganBaru.setDepth(5);
+            halanganBaru.setScale(0.85);
+
+            halanganBaru.setData('status_aktif', true);
+            halanganBaru.setData('kecepatan', obstacleSpeed);
+
+            this.halangan.push(halanganBaru);
+
+            this.timerHalangan = Phaser.Math.Between(this.minSpawnDelay, this.maxSpawnDelay);
+        };
+
+        this.gameOver = function () {
+            if (this.isGameOver) return;
+
+            this.isGameOver = true;
+            this.isGameRunning = false;
+
+            let highScore = Number(localStorage['highscore'] || 0);
+
+            if (this.score > highScore) {
+                localStorage['highscore'] = this.score;
+            }
+
+            this.snd_dead.play();
+
+            if (this.charaTweens != null) {
+                this.charaTweens.stop();
+            }
+
+            for (let i = 0; i < this.halangan.length; i++) {
+                this.halangan[i].setData('kecepatan', 0);
+            }
+
+            this.charaTweens = this.tweens.add({
+                targets: this.chara,
+                ease: 'Elastic.easeOut',
+                duration: 900,
+                alpha: 0,
+                angle: 180,
+                scaleX: 0,
+                scaleY: 0,
+                onComplete: function () {
+                    myScene.scene.start('sceneMenu');
+                }
+            });
+        };
     },
 
     update: function () {
-        if (this.isGameRunning) {
+        this.updateBackground();
 
-            // --- Keyboard jump ---
-            if (Phaser.Input.Keyboard.JustDown(this.cursors.up) ||
-                Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-                this.doJump();
+        if (!this.isGameRunning || this.isGameOver) return;
+
+        this.charaVelocity += this.gravity;
+
+        if (this.charaVelocity > this.maxFallSpeed) {
+            this.charaVelocity = this.maxFallSpeed;
+        }
+
+        if (this.charaVelocity < this.maxUpSpeed) {
+            this.charaVelocity = this.maxUpSpeed;
+        }
+
+        this.chara.y += this.charaVelocity;
+
+        let targetAngle = Phaser.Math.Clamp(this.charaVelocity * 4, -25, 35);
+        this.chara.angle = Phaser.Math.Linear(this.chara.angle, targetAngle, 0.12);
+
+        if (this.chara.y < this.topLimit) {
+            this.chara.y = this.topLimit;
+            this.gameOver();
+            return;
+        }
+
+        if (this.chara.y > this.bottomLimit) {
+            this.chara.y = this.bottomLimit;
+            this.gameOver();
+            return;
+        }
+
+        this.timerHalangan--;
+
+        if (this.timerHalangan <= 0) {
+            this.spawnObstacle();
+        }
+
+        for (let i = this.halangan.length - 1; i >= 0; i--) {
+            let obstacle = this.halangan[i];
+
+            obstacle.x -= obstacle.getData('kecepatan');
+
+            if (obstacle.x < -200) {
+                obstacle.destroy();
+                this.halangan.splice(i, 1);
+                continue;
             }
 
-            // --- Apply gravity each frame ---
-            this.velocityY += this.GRAVITY;
-            this.chara.y += this.velocityY;
-
-            // --- Ground collision ---
-            if (this.chara.y >= this.GROUND_Y) {
-                this.chara.y = this.GROUND_Y;
-                this.velocityY = 0;
-                this.isOnGround = true;
-            } else {
-                this.isOnGround = false;
+            if (
+                this.chara.x > obstacle.x + obstacle.displayWidth / 2 &&
+                obstacle.getData('status_aktif') === true
+            ) {
+                obstacle.setData('status_aktif', false);
+                this.score++;
+                this.label_score.setText(this.score.toString());
             }
 
-            // --- Ceiling / top boundary death ---
-            if (this.chara.y < 50) {
-                this.isGameRunning = false;
-                this.snd_dead.play();
-
-                var myScene = this;
-
-                this.tweens.add({
-                    targets: this.chara,
-                    ease: 'Elastic.easeOut',
-                    duration: 2000,
-                    alpha: 0,
-                    onComplete: function () {
-                        myScene.gameover();
-                    }
-                });
+            if (this.checkCollision(this.chara, obstacle)) {
+                obstacle.setData('status_aktif', false);
+                this.gameOver();
                 return;
             }
+        }
+    },
 
-            // --- Scroll background ---
-            for (let i = 0; i < this.background.length; i++) {
-                for (var j = 0; j < this.background[i].length; j++) {
-                    var bg = this.background[i][j];
-                    bg.x -= bg.getData('kecepatan');
-                    if (bg.x <= -1366 / 2) {
-                        bg.x += 1366 * 2;
-                    }
-                }
-            }
+    updateBackground: function () {
+        for (let i = 0; i < this.background.length; i++) {
+            for (let j = 0; j < this.background[i].length; j++) {
+                this.background[i][j].x -= this.background[i][j].getData('kecepatan');
 
-            // --- Spawn obstacles ---
-            if (this.timerHalangan == 0) {
-                var acak_y = Math.floor(Math.random() * 680 + 60);
-                var halanganbaru = this.add.image(1500, acak_y, 'obstc');
-                halanganbaru.setOrigin(0, 0);
-                halanganbaru.setData("status_aktif", true);
-                halanganbaru.setData("kecepatan", Math.floor(Math.random() * 8 + 5));
-                halanganbaru.setDepth(5);
-
-                this.halangan.push(halanganbaru);
-                this.timerHalangan = Math.floor(Math.random() * 50 + 10);
-            }
-
-            // --- Move & clean up obstacles ---
-            for (let i = this.halangan.length - 1; i >= 0; i--) {
-                var h = this.halangan[i];
-                h.x -= h.getData('kecepatan');
-                if (h.x < -200) {
-                    h.destroy();
-                    this.halangan.splice(i, 1);
-                    break;
-                }
-            }
-
-            this.timerHalangan--;
-
-            // --- Score counting ---
-            for (var i = this.halangan.length - 1; i >= 0; i--) {
-                var h = this.halangan[i];
-                if (this.chara.x > h.x + 50 && h.getData('status_aktif') == true) {
-                    h.setData('status_aktif', false);
-                    this.score++;
-                    this.label_score.setText(this.score);
-                }
-            }
-
-            // --- Collision detection ---
-            for (let i = this.halangan.length - 1; i >= 0; i--) {
-                if (this.chara.getBounds().contains(this.halangan[i].x, this.halangan[i].y)) {
-                    this.isGameRunning = false;
-                    this.snd_dead.play();
-
-                    var myScene = this;
-
-                    this.tweens.add({
-                        targets: this.chara,
-                        ease: 'Elastic.easeOut',
-                        duration: 2000,
-                        alpha: 0,
-                        onComplete: function () {
-                            myScene.gameover();
-                        }
-                    });
-                    break;
+                if (this.background[i][j].x <= -(1366 / 2)) {
+                    var diff = this.background[i][j].x + (1366 / 2);
+                    this.background[i][j].x = 1366 + 1366 / 2 + diff;
                 }
             }
         }
+    },
+
+    checkCollision: function (chara, obstacle) {
+        let charaBounds = chara.getBounds();
+        let obstacleBounds = obstacle.getBounds();
+
+        let charaHitbox = new Phaser.Geom.Rectangle(
+            charaBounds.x + charaBounds.width * 0.25,
+            charaBounds.y + charaBounds.height * 0.20,
+            charaBounds.width * 0.50,
+            charaBounds.height * 0.60
+        );
+
+        let obstacleHitbox = new Phaser.Geom.Rectangle(
+            obstacleBounds.x + obstacleBounds.width * 0.22,
+            obstacleBounds.y + obstacleBounds.height * 0.22,
+            obstacleBounds.width * 0.56,
+            obstacleBounds.height * 0.56
+        );
+
+        return Phaser.Geom.Intersects.RectangleToRectangle(charaHitbox, obstacleHitbox);
     }
 });
